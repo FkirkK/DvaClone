f92472875cf9d127699097ff4ae0e877dbb74271
Marc Beuchert <marcbeuchert@hotmail.com>
2017-09-20 11:42:16 +0200

    Added some classes for the importer

---
+++ Dva/Models/Nonwebsite related logic/Review.cs
@@ -0,0 +1,22 @@
+﻿namespace Dva.Models.Nonwebsite_related_logic
+{
+    public class Review
+    {
+        public bool Positive {
+            get;
+            set; 
+        }
+
+        public bool Truthful
+        {
+            get;
+            set;
+        }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+    }
+}
