ab49bb7d0e8dd3383871cb2aafc0558a2a1bf3a3
Unknown <stellehj@yahoo.dk>
2017-09-20 12:09:19 +0200

    Fixed wrong ref

--- DvaTesting/Models/Nonwebsite related/DataImporterTest.cs
+++ DvaTesting/Models/Nonwebsite related/DataImporterTest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
-using Dva.Models.Nonwebsite_related_logic;
+using Dva.Models.NonwebsiteRelated;
 
 namespace DvaTesting.Models.Nonwebsite_related
 {
