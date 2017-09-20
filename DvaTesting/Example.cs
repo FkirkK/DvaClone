8717f392397fc80756d76f0b7effae9f083cd238
Marc Beuchert <marcbeuchert@hotmail.com>
2017-09-20 11:42:01 +0200

    Fixed testing in project

---
+++ DvaTesting/Example.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace DvaTesting
+{
+    public class Example
+    {
+        [TestFixture]
+        public class AccountTest
+        {
+            [Test]
+            public void TransferFunds()
+            {
+                Assert.True(true);
+            }
+        }
+    }
+}
