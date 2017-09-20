1c85a7b6be0f198f97bfbf46825a5633364ffcb6
Unknown <stellehj@yahoo.dk>
2017-09-20 12:07:06 +0200

    Added test template for first test

--- DvaTesting/Models/Nonwebsite related/DataImporterTest.cs
+++ DvaTesting/Models/Nonwebsite related/DataImporterTest.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Dva;
 using NUnit.Framework;
+using Dva.Models.Nonwebsite_related_logic;
 
 namespace DvaTesting.Models.Nonwebsite_related
 {
     [TestFixture]
     class DataImporterTest
     {
-            [Test]
-            public void ShouldImportFilesCorrectly()
-            {
-                //Arrange
+        [Test]
+        public void ShouldThrowFileNotFoundException()
+        {
+            //Arrange
+            DataImporter di = new DataImporter();
 
-                //Act
-            }
+            //Act
+
+
+            //Assert
+            Assert.True(true);
+        }
     }
 }
