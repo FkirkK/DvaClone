0413a859dee9764240a019f27d3a3db98da26ee6
Matklem <mathiasklemmen@gmail.com>
2017-09-25 14:28:16 +0200

    Made tests and implemented Pythonrunner for integer functions

---
+++ DvaPythonRunner/PythonRunner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace DvaPythonRunner
+{
+    public class PythonRunner
+    {
+        //Based on example from MSDN: https://code.msdn.microsoft.com/windowsdesktop/C-and-Python-interprocess-171378ee
+        public object RunScript(string programString, params object[] input)
+        {
+            // full path of python interpreter 
+            string python = "../../../../Python/python.exe";
+
+            // Create new process start info 
+            ProcessStartInfo myProcessStartInfo = new ProcessStartInfo(python);
+
+            // make sure we can read the output from stdout 
+            myProcessStartInfo.UseShellExecute = false;
+            myProcessStartInfo.RedirectStandardOutput = true;
+
+            // start python app with X arguments  
+            // 1st arguments is pointer to itself
+            string callString = programString;
+
+            foreach (var param in input)
+            {
+                callString += " " + (int)param;
+            }
+
+            myProcessStartInfo.Arguments = callString;
+
+            Process myProcess = new Process();
+            // assign start information to the process 
+            myProcess.StartInfo = myProcessStartInfo;
+
+            // start the process 
+            myProcess.Start();
+
+            // Read the standard output of the app we called.  
+            // in order to avoid deadlock we will read output first 
+            // and then wait for process terminate: 
+            StreamReader myStreamReader = myProcess.StandardOutput;
+            string myString = myStreamReader.ReadLine();
+
+            /*if you need to read multiple lines, you might use: 
+                string myString = myStreamReader.ReadToEnd() */
+
+            // wait exit signal from the app we called and then close it. 
+            myProcess.WaitForExit();
+            myProcess.Close();
+
+            return myString;
+        }
+    }
+}
---
+++ DvaTesting/PythonRunnerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using DvaPythonRunner;
+
+namespace DvaTesting
+{
+    [TestFixture]
+    class PythonRunnerTest
+    {
+        [TestCase(1,2,3)]
+        [TestCase(4, 8, 12)]
+        [TestCase(5, 10, 15)]
+        [TestCase(100, 200, 300)]
+        public void DoesPythonReturnCorrectResult(int input1, int input2, int exceptedResult)
+        {
+
+            //Arrange
+            PythonRunner pr = new PythonRunner();
+
+            //Act
+            string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            int result = int.Parse((string)pr.RunScript(path+"\\python.py", input1, input2));
+
+            //Assert
+            Assert.AreEqual(result,exceptedResult);
+        }
+
+    }
+}
