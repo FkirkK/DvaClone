6493987672bbcd0db20ef3ff59b6b16969177739
Marc Beuchert <marcbeuchert@hotmail.com>
2017-10-05 10:21:50 +0200

    Test for Review ToString

---
+++ DvaTest/ReviewTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DvaCore.Models;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace DvaTest
+{
+
+    [TestFixture]
+    class ReviewTest {
+
+        [Test]
+        public void ToStringCorrectFormat()
+        {
+            //Arrange: 
+            Review review = new Review("Hello this should be equal.", "Title", true, true);
+            
+            //Assert:
+            Assert.AreEqual("Hello this should be equal.", review.ToString());
+        }
+        
+    }
+}
