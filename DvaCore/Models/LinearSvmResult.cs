5273a4987ceb0695a832cff61ab594dfdacd2cee
Marc Beuchert <marcbeuchert@hotmail.com>
2017-10-05 12:06:50 +0200

    Added a class to save LinearSvmResults in

---
+++ DvaCore/Models/LinearSvmResult.cs
@@ -0,0 +1,19 @@
+﻿namespace DvaCore.Models
+{
+    public class LinearSvmResult
+    {
+        private int _id;
+
+        public LinearSvmResult(int id)
+        {
+            _id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            LinearSvmResult linearSvmResult = (LinearSvmResult) obj;
+            
+            return linearSvmResult._id == _id;
+        }
+    }
+}
---
+++ DvaTest/AnalysisRunnerTest.cs
@@ -0,0 +1,25 @@
+﻿using DvaCore;
+using DvaCore.Models;
+using DvaPythonRunner;
+using NUnit.Framework;
+
+namespace DvaTest
+{
+    [TestFixture]
+    public class AnalysisRunnerTest
+    {
+        [Test]
+        public void RunLinearSvmReturnsCorrectLinearSvmResult()
+        {
+            //Arrange
+            IPythonRunner pr = new PythonRunner();
+            IAnalysisRunner ar = new AnalysisRunner(pr);
+
+            //Act
+            var linearSvmResult = ar.RunLinearSvm();
+
+            //Assert 
+            Assert.AreEqual(linearSvmResult, new LinearSvmResult(1));
+        }
+    }
+}
