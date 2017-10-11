4cd3ab396e1b500e21947df5c3faa87b45d90309
Stelle <stellehj@yahoo.dk>
2017-10-11 09:52:58 +0200

    Removed newlines and blank characters from dimensionalizer

--- DvaPythonScripts/OpinionSpamPackage/Dimensionalizer.py
+++ DvaPythonScripts/OpinionSpamPackage/Dimensionalizer.py
@@ -6,9 +6,17 @@ class Dimensionalizer:
     def __init__(self):
         self.dimensionSet = set()
 
-    def DimensionalizeReview(self, review):
+    def DimensionalizeReviewBigramPlus(self, review):
         splitContent = review.content.split(" ")
+        splitContent = self.__RemoveAllNewlines(splitContent)
         for i in range(0, len(splitContent)-1):
             self.dimensionSet.add(splitContent[i])
             self.dimensionSet.add(splitContent[i] + " " + splitContent[i+1])
         self.dimensionSet.add(splitContent[len(splitContent)-1])  # Done to get last element not caught in loop
+
+    def __RemoveAllNewlines(self, contentList):
+        returnList = []
+        for i in range(0, len(contentList)):
+            if contentList[i] != "\n" and contentList[i] != "":
+                returnList.append(contentList[i])
+        return returnList
