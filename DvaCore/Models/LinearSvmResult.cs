817bdc361c0a869f259e71dd15d8ee20740ce5f5
Mathias <mathiasbsvendsen@gmail.com>
2017-10-11 11:17:25 +0200

    Implemented parsing in LinearSvmResult

--- DvaCore/Models/LinearSvmResult.cs
+++ DvaCore/Models/LinearSvmResult.cs
@@ -1,19 +1,78 @@
-﻿namespace DvaCore.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DvaCore.Models
 {
     public class LinearSvmResult
     {
-        private int _id;
-
-        public LinearSvmResult(int id)
+        public LinearSvmResult(string inputToParse)
         {
-            _id = id;
+            Parse(inputToParse);
         }
+
+        public double OverallPrecision { get; private set; }
+        public double OverallBest { get; private set; }
+        public double OverallWorst { get; private set; }
+        public double FalsePositives { get; private set; }
+        public double FalsePositivesBest { get; private set; }
+        public double FalsePositivesWorst { get; private set; }
+        public double FalseNegatives { get; private set; }
+        public double FalseNegativesBest { get; private set; }
+        public double FalseNegativesWorst { get; private set; }
+        public IList<RatedDocument> RatedDocuments { get; private set; }
 
         public override bool Equals(object obj)
         {
-            LinearSvmResult linearSvmResult = (LinearSvmResult) obj;
+            var svmResult = (LinearSvmResult) obj;
+            var isSame = true;
+
+            isSame &= OverallPrecision.Equals(svmResult.OverallPrecision);
+            isSame &= OverallBest.Equals(svmResult.OverallBest);
+            isSame &= OverallWorst.Equals(svmResult.OverallWorst);
+            isSame &= FalsePositives.Equals(svmResult.FalsePositives);
+            isSame &= FalsePositivesBest.Equals(svmResult.FalsePositivesBest);
+            isSame &= FalsePositivesWorst.Equals(svmResult.FalsePositivesWorst);
+            isSame &= FalseNegatives.Equals(svmResult.FalseNegatives);
+            isSame &= FalseNegativesBest.Equals(svmResult.FalseNegativesBest);
+            isSame &= FalseNegativesWorst.Equals(svmResult.FalseNegativesWorst);
+            isSame &= RatedDocuments.Count == svmResult.RatedDocuments.Count;
             
-            return linearSvmResult._id == _id;
+            if (isSame)
+                for (int i = 0; i < RatedDocuments.Count; i++)
+                {
+                    isSame &= RatedDocuments[i].Equals(svmResult.RatedDocuments[i]);
+                }
+            
+            return isSame;
+        }
+
+        private void Parse(string input)
+        {
+            var splitInput = input.Split(',');
+
+            OverallPrecision = double.Parse(splitInput[0]);
+            OverallBest = double.Parse(splitInput[1]);
+            OverallWorst = double.Parse(splitInput[2]);
+            FalsePositives = double.Parse(splitInput[3]);
+            FalsePositivesBest = double.Parse(splitInput[4]);
+            FalsePositivesWorst = double.Parse(splitInput[5]);
+            FalseNegatives = double.Parse(splitInput[6]);
+            FalseNegativesBest = double.Parse(splitInput[7]);
+            FalseNegativesWorst = double.Parse(splitInput[8]);
+
+            var docList = new List<RatedDocument>();
+            for (int i = 9; i < splitInput.Length; i+=3)
+            {
+                string name = splitInput[i].Trim();
+                bool deceptive = splitInput[i + 1].Trim() == "1";
+                bool rating = splitInput[i + 2].Trim() == "1";
+                
+                docList.Add(new RatedDocument(name, deceptive, rating));
+            }
+
+            RatedDocuments = docList;
+
         }
     }
 }
---
+++ DvaTest/LinearSvmResultTest.cs
@@ -0,0 +1,83 @@
+﻿using DvaCore.Models;
+using NUnit.Framework;
+
+namespace DvaTest
+{
+    [TestFixture]
+    public class LinearSvmResultParserTest
+    {
+
+        [Test]
+        public void ParseStringNoDocs()
+        {
+            var input = "0.0, 1.0, 3.0, 2.0, 4.0, 5.5, 7.0, 0.0, 10.0";
+
+            var result = new LinearSvmResult(input);
+
+            Assert.AreEqual(result.OverallPrecision, 0.0);
+            Assert.AreEqual(result.OverallBest, 1.0);
+            Assert.AreEqual(result.OverallWorst, 3.0);
+            Assert.AreEqual(result.FalsePositives, 2.0);
+            Assert.AreEqual(result.FalsePositivesBest, 4.0);
+            Assert.AreEqual(result.FalsePositivesWorst, 5.5);
+            Assert.AreEqual(result.FalseNegatives, 7.0);
+            Assert.AreEqual(result.FalseNegativesBest, 0.0);
+            Assert.AreEqual(result.FalseNegativesWorst, 10.0);
+
+            Assert.AreEqual(result.RatedDocuments.Count, 0);
+        }
+        
+        [Test]
+        public void ParseStringTenDocs()
+        {
+            var input = "0.0, 1.0, 3.0, 2.0, 4.0, 5.5, 7.0, 0.0, 10.0, " +
+                        "doc1, 1, 1, doc2, 0, 1, doc3, 1, 1, doc4, 1, 1, " +
+                        "doc5, 1, 0, doc6, 1, 1, doc7, 0, 0, doc8, 0, 0, " +
+                        "doc9, 1, 0, doc10, 0, 1";
+
+            var result = new LinearSvmResult(input);
+
+            Assert.AreEqual(result.RatedDocuments.Count, 10);
+
+            var documents = result.RatedDocuments;
+            
+            Assert.AreEqual(new RatedDocument("doc1", true, true), documents[0]);
+            Assert.AreEqual(new RatedDocument("doc2", false, true), documents[1]);
+            Assert.AreEqual(new RatedDocument("doc3", true, true), documents[2]);
+            Assert.AreEqual(new RatedDocument("doc4", true, true), documents[3]);
+            Assert.AreEqual(new RatedDocument("doc5", true, false), documents[4]);
+            Assert.AreEqual(new RatedDocument("doc6", true, true), documents[5]);
+            Assert.AreEqual(new RatedDocument("doc7", false, false), documents[6]);
+            Assert.AreEqual(new RatedDocument("doc8", false, false), documents[7]);
+            Assert.AreEqual(new RatedDocument("doc9", true, false), documents[8]);
+            Assert.AreEqual(new RatedDocument("doc10", false, true), documents[9]);
+        }
+        
+        
+        [Test]
+        public void EqualityFlase()
+        {
+            var input = "0.0, 1.0, 3.0, 2.0, 4.0, 5.5, 7.0, 0.0, 10.0, " +
+                        "doc1, 1, 1, doc2, 0, 1, doc3, 1, 1, doc4, 1, 1, " +
+                        "doc5, 1, 0, doc6, 1, 1, doc7, 0, 0";
+
+            var result = new LinearSvmResult(input);
+            var result2 = new LinearSvmResult(input + ", doc8, 0, 1");
+
+            Assert.AreNotEqual(result, result2);
+        }
+        
+        [Test]
+        public void EqualityTrue()
+        {
+            var input = "0.0, 1.0, 3.0, 2.0, 4.0, 5.5, 7.0, 0.0, 10.0, " +
+                        "doc1,Equals 1, 1, doc2, 0, 1, doc3, 1, 1, doc4, 1, 1, " +
+                        "doc5, 1, 0, doc6, 1, 1, doc7, 0, 0";
+
+            var result = new LinearSvmResult(input + ", doc8, 0, 1");
+            var result2 = new LinearSvmResult(input + ", doc8, 0, 1");
+
+            Assert.AreEqual(result, result2);
+        }
+    }
+}
