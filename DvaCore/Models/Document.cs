73b972fb0a10b41ebeb1dba625bd7bb504d3cdcc
Mathias <mathiasbsvendsen@gmail.com>
2017-10-11 11:16:43 +0200

    Added documents class

---
+++ DvaCore/Models/Document.cs
@@ -0,0 +1,20 @@
+﻿namespace DvaCore.Models
+{
+    public class Document
+    {
+        public Document(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            var otherDoc = (Document) obj;
+
+            return Name.Equals(otherDoc.Name);
+        }
+    }
+}
---
+++ DvaCore/Models/RatedDocument.cs
@@ -0,0 +1,29 @@
+﻿namespace DvaCore.Models
+{
+    public class RatedDocument : Document
+    {
+        public RatedDocument(string name, bool deceptive, bool rating) : base(name)
+        {
+            Deceptive = deceptive;
+            Rating = rating;
+            
+        }
+
+        public bool Deceptive { get; set; }
+
+        public bool Rating { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            var otherDoc = (RatedDocument) obj;
+            var isSame = true;
+
+            isSame &= Deceptive.Equals(otherDoc.Deceptive);
+            isSame &= Rating.Equals(otherDoc.Rating);
+            isSame &= base.Equals(obj);
+
+            return isSame;
+        }
+    }
+}
