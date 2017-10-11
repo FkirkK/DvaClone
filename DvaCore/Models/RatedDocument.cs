3331f794649be27de3075c8b9ec88d7945631139
Marc Beuchert <marcbeuchert@hotmail.com>
2017-10-11 15:42:29 +0200

    Made summary to understand class

--- DvaCore/Models/RatedDocument.cs
+++ DvaCore/Models/RatedDocument.cs
@@ -2,15 +2,27 @@
 {
     public class RatedDocument : Document
     {
+        /// <summary>
+        /// Constructor for RatedDocument
+        /// </summary>
+        /// <param name="name">Name of the document</param>
+        /// <param name="labeledClassifier">Describes wether the document is labeled as deceptive</param>
+        /// <param name="ourClassifier">Describes wether our algorithm detects the document as deceptive</param>
         public RatedDocument(string name, bool labeledClassifier, bool ourClassifier) : base(name)
         {
             LabeledClassifier = labeledClassifier;
             OurClassifier = ourClassifier;
         }
 
-        public bool LabeledClassifier { get; set; } //If this is true the source says that the rating is deceptive
+        /// <summary>
+        /// If this is true the source says that the rating is deceptive
+        /// </summary>
+        public bool LabeledClassifier { get; set; }     
 
-        public bool OurClassifier { get; set; } //If this is true our algorithm says that the rating is deceptive
+        /// <summary>
+        /// If this is true our algorithm says that the rating is deceptive
+        /// </summary>
+        public bool OurClassifier { get; set; }         
 
         public override bool Equals(object obj)
         {
