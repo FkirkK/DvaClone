7b9e1ed149db80b2b0023866240755f41579f28b
Marc Beuchert <marcbeuchert@hotmail.com>
2017-10-11 14:58:15 +0200

    Added AcceptanceTest for U6

---
+++ DvaTest/AcceptanceTest/U6Test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace DvaTest.AcceptanceTest
+{
+    [TestFixture]
+    class U6Test
+    {
+        /* 1
+         * This is a sample test, testing one aspect of the userstory
+         * TODO: Actually make a test
+         */
+        [Test]
+        public void SampleTest()
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.True(true);
+        }
+    }
+}
