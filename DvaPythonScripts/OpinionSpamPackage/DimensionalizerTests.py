75451a28b4216e255c6b188a4e6167d5f54bc5e6
Stelle <stellehj@yahoo.dk>
2017-10-11 10:20:31 +0200

    Renamed python tests

--- DvaPythonScripts/OpinionSpamPackage/DimensionalizerTests.py
+++ DvaPythonScripts/OpinionSpamPackage/DimensionalizerTests.py
@@ -10,19 +10,17 @@ class DimensionalizerTests(TestCase):
         self.osReader = OpSpamReader()
         self.readReviews = self.osReader.ReadAllFiles()
 
-    def test_dimensionalizes_file_1_correctly(self):
+    def test_bigramplus_file_1_correctly(self):
         expectedSetResult = 195
         self.dimensionalizer.DimensionalizeReviewBigramPlus(self.readReviews[0])
-        print(self.dimensionalizer.dimensionSet)
         self.assertEqual(len(self.dimensionalizer.dimensionSet), expectedSetResult)
 
-    def test_dimensionalizes_file_11_correctly(self):
+    def test_bigramplus_file_82_correctly(self):
         expectedSetResult = 180
         self.dimensionalizer.DimensionalizeReviewBigramPlus(self.readReviews[82])
-        print(self.dimensionalizer.dimensionSet)
         self.assertEqual(len(self.dimensionalizer.dimensionSet), expectedSetResult)
 
-    def test_dimensionalizes_file_1_and_11_combined_correctly(self):
+    def test_bigramplus_file_1_and_82_combined_correctly(self):
         expectedTwoSetsResult = 363
         self.dimensionalizer.DimensionalizeReviewBigramPlus(self.readReviews[0])
         self.dimensionalizer.DimensionalizeReviewBigramPlus(self.readReviews[82])
