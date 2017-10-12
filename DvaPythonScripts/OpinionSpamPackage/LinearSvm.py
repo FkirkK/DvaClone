7aa6a8084e8a5e3fe5b9fc0bc2a5fb982f9debdb
Frederik Kirk Kristensen <Frederik_KKristensen@hotmail.com>
2017-10-12 11:07:16 +0200

    :snake: Changed test script to run actual svm :snake:

--- DvaPythonScripts/OpinionSpamPackage/LinearSvm.py
+++ DvaPythonScripts/OpinionSpamPackage/LinearSvm.py
@@ -1,3 +1,11 @@
-import sys 
- 
-print("Done")
+import os
+from OpinionSpamPackage import DvaLinearSvm
+from OpinionSpamPackage import OpSpamReader
+
+osReader = OpSpamReader(opSpamFolderPath=os.path.dirname(os.path.dirname(os.getcwd())) + "\\op_spam_v1.4")
+readReviews = osReader.ReadAllFiles()
+
+DvaSVM = DvaLinearSvm(readReviews)
+classifierResult = DvaSVM.Do5FoldValidation()
+
+print(classifierResult)
