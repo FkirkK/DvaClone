4ce7f29109fb1b557a67e19cf5f800c021b6303b
Stelle <stellehj@yahoo.dk>
2017-10-19 15:24:36 +0200

    Renamed DvaWeb to DvaWebApp

---
+++ DvaWebApp/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using DvaCore;
+using Microsoft.AspNetCore.Mvc;
+using DvaCore.Models;
+using DvaDataImporter;
+using DvaPythonRunner;
+using DvaWeb.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DvaWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult LinearSVM()
+        {
+            //ViewBag.MethodName = new SelectList(new[] {
+            //"LinearSVMResult", "LinearSVMResult1", "LinearSVMResult2"});
+            //return View(new DummyModel());
+            return View();
+            }
+
+        [HttpPost]
+        public IActionResult LinearSVMResult(string methodName)
+        {
+            IAnalysisRunner runner = new AnalysisRunner(new PythonRunner(), new Judge());
+            IResult result = runner.RunLinearSvmBigram();
+            ViewBag.LinearSvmResult = result;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LinearSVMResult1()
+        {
+            IAnalysisRunner runner = new AnalysisRunner(new PythonRunner(), new Judge());
+            //IResult result = runner.RunLinearSvm();
+            //ViewBag.LinearSvmResult = result;
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LinearSVMResult2()
+        {
+            IAnalysisRunner runner = new AnalysisRunner(new PythonRunner(), new Judge());
+            //IResult result = runner.RunLinearSvm();
+            //ViewBag.LinearSvmResult = result;
+
+            return View();
+        }
+
+        public IActionResult Eksempel(int firstnumber = 0, int secondnumber = 0)
+        {
+            ViewData["Message"] = "Your application description page.";
+            int result = firstnumber + secondnumber;
+            ViewBag.res = result;
+
+            DataImporter di = new DataImporter();
+            ViewBag.reviews = di.ImportReviews(@"C:\Users\marcb\Documents\GitHub\Dva\op_spam_v1.4");
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
