c79518b823fbba5a682a923b68006dfc6430837c
Marc Beuchert <marcbeuchert@hotmail.com>
2017-10-26 11:24:41 +0200

    Fixed DropDown menu -- I ist best UI programmer

---
+++ DvaWebApp/Models/AlgorithmSettingsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DvaWebApp.Models
+{
+    public class AlgorithmSettingsModel
+    {
+        public SelectList AlgorithmList { get; set; }
+        public string SelectedAlgorithm { get; set; }
+    }
+}
