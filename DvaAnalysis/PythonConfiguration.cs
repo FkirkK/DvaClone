57e1c6035ec1e83bb05f187ad364b391916737a5
Stelle <stellehj@yahoo.dk>
2017-10-30 11:38:00 +0100

    Added tests for python configuration
    To be used later

---
+++ DvaAnalysis/PythonConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DvaAnalysis
+{
+    public class PythonConfiguration : AnalysisConfiguration
+    {
+        public PythonConfiguration(Classification clas, BagOfWords bow)
+        {
+            Classification = clas;
+            BagOfWords = bow;
+        }
+
+        public Classification Classification { get; }
+
+        public BagOfWords BagOfWords { get; }
+    }
+}
---
+++ DvaTest/UnitTest/PythonConfigurationTest.cs
@@ -0,0 +1,29 @@
+﻿using DvaAnalysis;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DvaTest.UnitTest
+{
+    [TestFixture]
+    class PythonConfigurationTest
+    {
+        [TestCase(Classification.LinearSVC, BagOfWords.Bigram)]
+        [TestCase(Classification.SVCLinear, BagOfWords.BigramPlus)]
+        [TestCase(Classification.SVCPolynomial, BagOfWords.Trigram)]
+        [TestCase(Classification.SVCRbf, BagOfWords.TrigramPlus)]
+        [TestCase(Classification.SVCSigmoid, BagOfWords.Unigram)]
+        public void CanSetConfiguration(Classification clas, BagOfWords bow)
+        {
+            //Arrange
+            PythonConfiguration pc = new PythonConfiguration(clas, bow);
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(clas, pc.Classification);
+            Assert.AreEqual(bow, pc.BagOfWords);
+        }
+    }
+}
