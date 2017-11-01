347f00c1921fcd65002bcbb21551d48700c0b5df
Marc Beuchert <marcbeuchert@hotmail.com>
2017-11-01 13:15:28 +0100

    Added Javascript for analysis

---
+++ DvaWebApp/wwwroot/js/DeceptionAnalysisScript.js
@@ -0,0 +1,19 @@
+﻿$(document).on('change', '#FeatureDropDown', function () {
+    var FeatureValue = $('#FeatureDropDown').val();
+    var ClassificationValue = $('#ClassificationDropDown').val();
+
+    if (FeatureValue !== "" && ClassificationValue !== "")
+        $('#Submit').removeAttr('disabled');
+    else 
+        $('#Submit').attr('disabled', 'disabled');
+});
+
+$(document).on('change', '#ClassificationDropDown', function () {
+    var FeatureValue = $('#FeatureDropDown').val();
+    var ClassificationValue = $('#ClassificationDropDown').val();
+
+    if (FeatureValue !== "" && ClassificationValue !== "")
+        $('#Submit').removeAttr('disabled');
+    else
+        $('#Submit').attr('disabled', 'disabled');
+});
